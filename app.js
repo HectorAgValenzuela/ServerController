a83f4f8787440f944fb4ed25e7aa9a2be300197c
HectorAg1 <hector-av1@hotmail.com>
2024-07-04 00:27:39 -0600

    reset botton and logs

--- app.js
+++ app.js
@@ -4,23 +4,25 @@
 
 const { Client } = require('ssh2');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const cors = require('cors');
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+let output = '';
 
 // Conectar a la instancia VM de Oracle
 function connectToOracleVM(command) {
   const conn = new Client();
-  
   return new Promise((resolve, reject) => {
     conn.on('ready', () => {
       console.log('Conexión SSH establecida');
       conn.shell((err, stream) => {
         if (err) return reject(err);
   
-        let output = '';
         stream.on('close', (code, signal) => {
           resolve({ code, signal, output });
         }).on('data', (data) => {
@@ -69,6 +71,22 @@ app.post('/apagar', async (req, res) => {
     console.error('Error al apagar el servidor:', error);
     res.status(500).send('Error al apagar el servidor.');
   }
+});
+
+app.post('/reiniciar', async (req, res) => {
+  try {
+    
+    const command = `sudo reboot`;
+    await connectToOracleVM(command);
+    res.send('Servidor apagado correctamente.');
+  } catch (error) {
+    console.error('Error al apagar el servidor:', error);
+    res.status(500).send('Error al apagar el servidor.');
+  }
+});
+
+app.get('/console-output', (req, res) => {
+  res.send(output);
 });
 
 // Iniciar el servidor web
